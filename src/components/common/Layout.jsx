85a9763f44861f87ebdb3d4ede6cd6ab60224025
Jueun Lee <leeje7766@gmail.com>
2024-04-04 13:43:10 +0900

    ✨ Added routing to Tree page

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -2,14 +2,11 @@ import { Outlet } from 'react-router-dom';
 import useAuth from '../hooks/useAuth';
 // 전체 스타일 여기서 적용
 
-const Layout = ({ children }) => {
+const Layout = () => {
   return (
-    <div className="bg-black">
-      <div className="bg-white min-h-screen max-w-500px mx-auto tracking-tight">
-        {children}
-      </div>
+    <div className="bg-bgcolor w-screen h-screen max-w-screen-sm">
+      <Outlet />
     </div>
   );
 };
-
 export default Layout;
