6926003c6345ba4bc2ed1f77a39dec3b9d7f799d
hbjun <gosqlsz@gmail.com>
2024-04-04 12:17:07 +0900

    🎨 improve of structure - layout & folder tree

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 // 전체 스타일 여기서 적용
 
 const Layout = () => {
   return (
-    <div className="bg-bgcolor w-screen h-screen max-w-screen-sm w-full h-full">
+    <div className="bg-bgcolor w-screen h-screen max-w-screen-sm">
       <Outlet />
     </div>
   );
