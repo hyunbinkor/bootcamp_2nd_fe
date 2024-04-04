9c84d58d4f09f2798884b17ab7818f1815025a1c
Hyunbin Jun <gosqlsz@gmail.com>
2024-04-04 12:17:07 +0900

    🎨 improve of structure - layout & folder tree

--- src/components/atom/Buttons.jsx
+++ src/components/atom/Buttons.jsx
@@ -1,9 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 const KakaoButton = (props) => {
-  return <button className="btn btn-warning w-full">카카오로 로그인</button>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(props.redirect);
+  };
+  return (
+    <button onClick={handleClick} className="btn btn-warning w-full">
+      카카오로 로그인
+    </button>
+  );
 };
 
 const NaverButton = (props) => {
-  return <button className="btn btn-success w-full">네이버로 로그인</button>;
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate(props.redirect);
+  };
+  return (
+    <button onClick={handleClick} className="btn btn-success w-full">
+      네이버로 로그인
+    </button>
+  );
 };
 
 export { KakaoButton, NaverButton };
