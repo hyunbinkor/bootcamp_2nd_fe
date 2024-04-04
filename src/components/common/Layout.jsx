89ab77bc5e64f577c477277f989ca9f5b460a2f6
Jueun Lee <leeje7766@gmail.com>
2024-04-04 15:03:55 +0900

    🔀 merge develop branch

--- src/components/common/Layout.jsx
+++ src/components/common/Layout.jsx
@@ -3,9 +3,12 @@ import useAuth from '../hooks/useAuth';
 // 전체 스타일 여기서 적용
 
 const Layout = () => {
+  const { isAuth, isLoading } = useAuth();
   return (
-    <div className="bg-bgcolor w-screen h-screen max-w-screen-sm">
-      <Outlet />
+    <div className="bg-bgcolor w-screen flex justify-center">
+      <div className="h-screen w-screen max-w-screen-sm">
+        <Outlet />
+      </div>
     </div>
   );
 };
