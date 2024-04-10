229a96af8dd34fd3a37bb688323581107654f207
Hyunbin Jun <gosqlsz@gmail.com>
2024-04-10 15:02:14 +0900

    📝 Questions Update

--- src/components/hooks/useAxios.jsx
+++ src/components/hooks/useAxios.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+// 질문 1. 여기서도 하나의 인스턴스로 보내는 것이 더 나아보이는데 그렇게 해도 되는것인지?
+// 질문 2. Loading context를 만들어뒀는데 그를 그냥 여기서 사용하는 것이 제일 깔끔할 것 같은데 맞는지?
+// 질문 3. 에러 분기를 한 번에 모아서 처리하는건 그냥 promise니까 ErrorBounday 컴포넌트에서 잡으면 되는건지? 그렇다면 status와 message 전달을 어떻게 하는것인지?
+// -> context를 이용하기에는 error의 경우 따로 방안이 마련되어 있을 것 같다는 느낌.
+
 const useAxios = (configParams) => {
   axios.defaults.baseURL = import.meta.env.VITE_API_URL;
   const [res, setRes] = useState('');
