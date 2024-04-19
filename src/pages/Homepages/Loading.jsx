f9ba4de71e3833744f45ce8d1527f2116d6d76c3
Hyunbin Jun <gosqlsz@gmail.com>
2024-04-19 14:10:52 +0900

    💄 Loading update

--- src/pages/Homepages/Loading.jsx
+++ src/pages/Homepages/Loading.jsx
@@ -3,7 +3,7 @@ import { Title } from '../../components/atom/Titles';
 const Load = () => {
   console.log('mount loading');
   return (
-    <div className="flex flex-col w-full h-full max-w-screen-sm px-14 py-12">
+    <div className="bg-bgcolor w-screen h-screen justify-center z-10 flex flex-col max-w-screen-sm px-14 py-12">
       <div className="h-1/2" />
       <Title text="로딩중" className="flex jusifty-center" />
       <div className="h-1/2" />
